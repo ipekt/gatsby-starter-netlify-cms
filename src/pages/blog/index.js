3d5f135cad0f77fb4bf93371234616e146992cc9
ipek turgul <ipek.turgul@gmail.com>
2020-11-22 10:46:57 +1100

    update links

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,8 +16,8 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #6DA34D, -0.5rem 0 0 #6DA34D',
-              backgroundColor: '#6DA34D',
+              boxShadow: '0.5rem 0 0 rgb(239 0 109), -0.5rem 0 0 rgb(239 0 109)',
+              backgroundColor: 'rgb(239 0 109)',
               color: 'white',
               padding: '1rem',
             }}
